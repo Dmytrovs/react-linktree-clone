dae47fc24518b79b02f5e605057741128022796a
Dmytry <dmytry@gmail.com>
2026-05-09 10:55:35 +0300

    fix: set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://dmytrovs.github.io/react-linktree-clone/',
    server: {
     open: true,
     browser: 'msedge', 
